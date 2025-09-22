92cc1f422c1a8a3bb1afa183d8ffb3d6b0924dea
kaviska <126580314+kaviska@users.noreply.github.com>
2025-09-22 20:54:14 +0530

    testing file done

---
+++ erp-system/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'erp-system';
+  malidu="malidu"
+}
+  
